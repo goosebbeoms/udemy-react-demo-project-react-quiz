6997ad616c4a4ecbb2d27bb74a90537a9a3b3d64
goosebbeoms <seo980916@gmail.com>
2024-07-05 22:04:33 +0900

    194. 답변 셔플 & 퀴즈 로직 추가

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,35 @@
 import { useState } from "react";
 import QUESTIONS from '../questions.js'
+import quizCompleteImg from '../assets/quiz-complete.png'
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([])
 
   const activeQuestionIndex = userAnswers.length
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
   function handleSeletAnswer(selectedAnswer) {
     setUserAnswers(prevUserAnswers => [...prevUserAnswers, selectedAnswer])
   }
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    )
+  }
+
+  const suffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+  suffledAnswers.sort(() => Math.random() - 0.5)
 
   return (
     <div id="quiz">
       <div id="question">
         <p>{QUESTIONS[activeQuestionIndex].text}</p>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+          {suffledAnswers.map(answer => (
             <li key={answer} className="answer">
               <button onClick={() => handleSeletAnswer(answer)}>{answer}</button>
             </li>
