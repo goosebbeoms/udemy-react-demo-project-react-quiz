988ebd6e4b2b078c7c589b532bb074ad2a022677
goosebbeoms <seo980916@gmail.com>
2024-07-06 02:05:16 +0900

    195. 질문 타이머 추가

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+
 import QUESTIONS from '../questions.js'
+import QuestionTimer from "./QuestionTimer.jsx";
 import quizCompleteImg from '../assets/quiz-complete.png'
 
 const Quiz = () => {
@@ -8,7 +10,7 @@ const Quiz = () => {
   const activeQuestionIndex = userAnswers.length
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
-  function handleSeletAnswer(selectedAnswer) {
+  function handleSelectAnswer(selectedAnswer) {
     setUserAnswers(prevUserAnswers => [...prevUserAnswers, selectedAnswer])
   }
 
@@ -27,11 +29,12 @@ const Quiz = () => {
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)} />   {/* 해당 질문에 어떠한 답도 선택되지 않았다고 알려주는 placeholder */}
         <p>{QUESTIONS[activeQuestionIndex].text}</p>
         <ul id="answers">
           {suffledAnswers.map(answer => (
             <li key={answer} className="answer">
-              <button onClick={() => handleSeletAnswer(answer)}>{answer}</button>
+              <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
             </li>
           ))}
         </ul>
