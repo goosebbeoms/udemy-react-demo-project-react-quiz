797fc49c477a8ed57374c150c580b8658fe77f9f
goosebbeoms <seo980916@gmail.com>
2024-07-07 13:18:36 +0900

    202. 퀴즈 결과 출력하기

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from "react"
 
 import QUESTIONS from "../questions.js"
-import quizCompleteImg from "../assets/quiz-complete.png"
 import Question from "./Question.jsx"
+import Summary from "./Summary.jsx"
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([])
@@ -21,10 +21,7 @@ const Quiz = () => {
 
   if (quizIsComplete) {
     return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz Completed!</h2>
-      </div>
+      <Summary userAnswers={userAnswers} />
     )
   }
 
